95c8f4293429a9f7b9eb3f219615351d6999d3ac
Abhinav <rawat.abhinav.2017446@gmail.com>
2023-04-04 18:38:56 +0530

    Deployement settings added

---
+++ main.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const express = require('express');
+const router = require('./routes');
+const { MongoClient } =require('mongodb');
+const { config } =require('dotenv');
+var tools = require('./dbUtils/databaseConn.js');
+const cors = require('cors')
+
+
+
+tools.InitDbConn();
+
+
+const PORT = process.env.PORT || 4500;
+config();
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000",
+}))
+
+// if(process.env.NODE_ENV == "production")
+// {
+//     app.use(express.static("frontend/build"));
+// }
+
+//Importing Router
+app.use('/api', router)
+
+app.use(express.static("frontend/build"));
+
+app.get("*",function(_,res){
+    res.sendFile(
+        path.join(__dirname,"./frontend/build/index.html"),
+        (err)=>{
+            res.status(500).send(err)
+        }
+    );
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Started at ${PORT}`)
+})
+
+
